ae37ea17133d1c1cd213fc84de261d9f14c8166b
Jhonny Fialho <jhonny@1creativetech.com>
2026-04-07 22:00:17 -0300

    feat: update test user credentials and allow GET requests for NFC-e imports

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\NFController;
 use App\Http\Controllers\NfceImportController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('nf/consultar', [NFController::class, 'consultar'])->name('nf.consultar');
-Route::post('nfce/importar', [NfceImportController::class, 'importar'])->name('nfce.importar');
+Route::match(['get', 'post'],'nfce/importar', [NfceImportController::class, 'importar'])->name('nfce.importar');
