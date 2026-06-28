b6fd98b5bda0ac10667c86c485e954645772b04a
Jhonny <jhonny@1creativetech.com>
2026-06-28 11:33:43 -0300

    feat: Set Sanctum token expiration to 30 days and schedule pruning of expired tokens

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Remove tokens Sanctum expirados (retém os últimos 30 dias = 720h)
+Schedule::command('sanctum:prune-expired --hours=720')->daily();
