053c82ac49115f8804512b5a2f07e03b7823eccd
Jhonny <jhonny@1creativetech.com>
2026-07-17 21:06:12 -0300

    fix: remove redundant button elements in mobile layout for cleaner markup

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -73,11 +73,7 @@
                             <button class="kt-btn kt-btn-icon kt-btn-ghost" data-kt-drawer-toggle="#sidebar">
                                 <i class="ki-filled ki-menu">
                                 </i>
-                            </button>
-                            <button class="kt-btn kt-btn-icon kt-btn-ghost" data-kt-drawer-toggle="#mega_menu_wrapper">
-                                <i class="ki-filled ki-burger-menu-2">
-                                </i>
-                            </button>
+                            </button>                            
                         </div>
                     </div>
                     <!-- End of Mobile Logo -->
