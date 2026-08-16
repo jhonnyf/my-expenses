438cbea2506c71107f8af52135b7c8d4e1c15c24
Jhonny <jhonny@1creativetech.com>
2026-08-16 19:00:55 -0300

    Update asset filenames in manifest and change cache name in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cestazen-static-b292774593';
+const CACHE_NAME = 'cestazen-static-91e654a06a';
 const STATIC_ASSET_PATTERN = /^\/(assets|build)\//;
 
 self.addEventListener('install', () => {
