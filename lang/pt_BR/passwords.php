e7a2b9b49bbfcf2d529ebbc8d1c7236cead378fa
Jhonny <jhonny@1creativetech.com>
2026-09-25 22:23:39 -0300

    feat: add Portuguese language support for authentication, pagination, passwords, and validation messages

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => 'Sua senha foi redefinida.',
+    'sent' => 'Enviamos o link de redefinição de senha para o seu e-mail.',
+    'throttled' => 'Aguarde um pouco antes de tentar novamente.',
+    'token' => 'Este link de redefinição de senha é inválido ou expirou.',
+    'user' => 'Não encontramos um usuário com esse e-mail.',
+
+];
